1282ba65c65de185aad94e5b26e9deae033035db
haoxiaofan <569192072@qq.com>
2018-01-30 17:19:53 +0800

    修改报错并添加描述特性

--- NewLibCore.Data/SQL/BuildExtension/JoinType.cs
+++ NewLibCore.Data/SQL/BuildExtension/JoinType.cs
@@ -1,11 +1,16 @@
-﻿namespace NewLibCore.Data.SQL.BuildExtension
+﻿using System.ComponentModel;
+
+namespace NewLibCore.Data.SQL.BuildExtension
 {
     public enum JoinType
     {
+        [Description("Inner")]
         Inner = 1,
 
+        [Description("Left")]
         Left = 2,
 
+        [Description("Left")]
         Right = 3
     }
 }
--- NewLibCore.Data/SQL/InternalDataStore/JoinManager.cs
+++ NewLibCore.Data/SQL/InternalDataStore/JoinManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NewLibCore.Data.SQL.BuildExtension;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 
