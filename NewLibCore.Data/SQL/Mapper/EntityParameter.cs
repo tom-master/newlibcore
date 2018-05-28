4fb4ad248c5f062a7a253f7f95f1e8b3bca816c8
haoxiaofan <569192072@qq.com>
2018-05-28 10:50:16 +0800

    add commit

--- NewLibCore.Data/SQL/Mapper/EntityParameter.cs
+++ NewLibCore.Data/SQL/Mapper/EntityParameter.cs
@@ -31,7 +31,7 @@ namespace NewLibCore.Data.SQL.Mapper
             var parameter = MapperFactory.Mapper.GetParameterInstance();
             parameter.ParameterName = entityParameter.Key;
             parameter.Value = entityParameter.Value;
-            return parameter;
+            return parameter; 
         }
 
         private Object ParseValueType(Object obj)
