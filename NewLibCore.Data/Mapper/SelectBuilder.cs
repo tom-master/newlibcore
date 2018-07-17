e3afbb6dfa6a4e7615ac7bc94bdd2bb815e912f3
zifan.hao <569192072@qq.com>
2018-07-17 17:07:47 +0800

    w

--- NewLibCore.Data/Mapper/SelectBuilder.cs
+++ NewLibCore.Data/Mapper/SelectBuilder.cs
@@ -19,6 +19,21 @@ namespace NewLibCore.Data.Mapper
 			return null;
 		}
 
-		
+		public SelectBuilder(TModel model) : base(model) { }
+
+		public SelectBuilder<TModel> LeftJoin<SlaveModel>(Expression<Func<SlaveModel>> leftExpression) where SlaveModel : class, new()
+		{
+			return this;
+		}
+
+		public SelectBuilder<TModel> RightJoin<SlaveModel>(Expression<Func<SlaveModel>> rightExpression) where SlaveModel : class, new()
+		{
+			return this;
+		}
+
+		public SelectBuilder<TModel> InnerJoin<SlaveModel>(Expression<Func<SlaveModel>> innerExpression) where SlaveModel : class, new()
+		{
+			return this;
+		}
 	}
 }
