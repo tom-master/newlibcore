81df24f79c8002ede849d35f1cff884218b6fc9a
zifan.hao <569192072@qq.com>
2018-07-19 10:12:40 +0800

    w

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -75,40 +75,32 @@ namespace UnitTestProject1
 			//	});
 			//});
 
-			//using (var dataStore = new DataStore("Server=39.106.106.137;Database=NewCrmContext;Uid=root;Pwd=xiaofan@.1;port=6033;SslMode=none;Pooling=True;Min Pool Size=5;Max Pool Size=10;Treat Tiny As Boolean=false"))
-			//{
-			//	try
-			//	{
-			//		dataStore.OpenTransaction();
-			//		#region 设置用户下线
-			//		{
-			//			var account = new Account();
-			//			account.Offline();
-			//			var a1 = "str";
-			//			var rowCount = dataStore.ExecuteModify(account, acc => acc.Id == 4 || acc.Name.Contains(a1) && !acc.IsDeleted && !acc.IsDisable);
-			//			if (rowCount == 0)
-			//			{
-
-			//			}
-			//		}
-			//		#endregion
-
-			//		dataStore.Commit();
-			//	}
-			//	catch (Exception e)
-			//	{
-			//		dataStore.Rollback();
-			//		throw;
-			//	}
-			//}
-
-			using (var dataStore = new DataStore(""))
+			using (var dataStore = new DataStore("Server=39.106.106.137;Database=NewCrmContext;Uid=root;Pwd=xiaofan@.1;port=6033;SslMode=none;Pooling=True;Min Pool Size=5;Max Pool Size=10;Treat Tiny As Boolean=false"))
 			{
-				dataStore.Query<Account, Account2>((a, b) => a.Name == b.Name && a.IsDeleted == b.IsDeleted);
-			}
+				try
+				{
+					dataStore.OpenTransaction();
+					#region 设置用户下线
+					{
+						var account = new Account();
+						account.Offline();
+						var a1 = "str";
+						var rowCount = dataStore.ExecuteModify(account, acc => acc.Id == 4 || acc.Name.Contains(a1) && !acc.IsDeleted && !acc.IsDisable);
+						if (rowCount == 0)
+						{
 
-			//var queryBuilder = new QueryBuilder<Account>();
-			Expression<Func<Account, Object>> expression = a => a.IsDeleted;
+						}
+					}
+					#endregion
+
+					dataStore.Commit();
+				}
+				catch (Exception e)
+				{
+					dataStore.Rollback();
+					throw;
+				}
+			}
 		}
 
 
