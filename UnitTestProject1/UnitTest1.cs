6942313df6fb02d5af004ec9de4d673a940e473e
zifan.hao <569192072@qq.com>
2018-07-26 10:52:59 +0800

    w

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -15,49 +15,13 @@ using NewLibCore.Data.Mapper.PropertyExtension;
 
 namespace UnitTestProject1
 {
-	[TestClass]
-	public class UnitTest1
-	{
-		[TestMethod] 
-		public void TestMethod1()
-		{
-<<<<<<< HEAD
-			var account = new Account();
-			var result = account.GetType().GetProperties().Where(w => w.PropertyType.Name != "IList`1" && w.GetCustomAttributes<ValidateBase>().Any());
-			var a = 0;
-			//using (var dataStore = new DataStore("Server=39.106.106.137;Database=NewCrmContext;Uid=root;Pwd=xiaofan@.1;port=6033;SslMode=none;Pooling=True;Min Pool Size=5;Max Pool Size=10;Treat Tiny As Boolean=false"))
-			//{
-			//	try
-			//	{
-			//		dataStore.OpenTransaction();
-			//		#region 设置用户下线
-			//		{
-			//			var account = new Account();
-			//			dataStore.ExecuteAdd(account);
-			//		}
-			//		#endregion
-=======
-			using (var dataStore = new DataStore("Server=39.106.106.137;Database=NewCrmContext;Uid=root;Pwd=xiaofan@.1;port=6033;SslMode=none;Pooling=True;Min Pool Size=5;Max Pool Size=10;Treat Tiny As Boolean=false"))
-			{
-				try
-				{
-					dataStore.OpenTransaction();
-					#region 设置用户下线
-					{ 
-						var account = new Account();
-						dataStore.ExecuteAdd(account);
-					}
-					#endregion
->>>>>>> fb5f71a6703dcb07ca452b71bdf15c40f4d934e8
-
-			//		dataStore.Commit();
-			//	}
-			//	catch (Exception e)
-			//	{
-			//		dataStore.Rollback();
-			//		throw;
-			//	}
-			//}
-		}
-	}
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+          
+        }
+    }
 }
