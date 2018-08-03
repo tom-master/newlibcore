2ca01e8da173faec25537bf9a386db2e2f6d39af
zifan.hao <569192072@qq.com>
2018-08-03 14:28:58 +0800

    w

--- NewLibCore/EnumExtensions.cs
+++ NewLibCore/EnumExtensions.cs
@@ -2,25 +2,27 @@
 
 namespace NewLibCore
 {
-	public sealed class EnumExtensions
-    {
-        public static T ToEnum<T>(String value) where T : struct
-        {
-            T t;
-            if (Enum.TryParse(value, true, out t))
-            {
-                return t;
-            }
+	public static class EnumExtensions
+	{
+		public static T ToEnum<T>(String value) where T : struct
+		{
+			T t;
+			if (Enum.TryParse(value, true, out t))
+			{
+				return t;
+			}
 
-            throw new ArgumentException($"{value}不是有效的类型");
-        }
+			throw new ArgumentException($"{value}不是有效的类型");
+		}
 
-        /// <summary>
-        /// 参数转换为枚举类型
-        /// </summary>
-        public static T ToEnum<T>(Int32 value) where T : struct
-        {
-            return (T)Enum.ToObject(typeof(T), value);
-        }
-    }
+		public static T ToEnum<T>(Int32 value) where T : struct
+		{
+			return (T)Enum.ToObject(typeof(T), value);
+		}
+
+		public static Int32 ToInt32(this Enum e)
+		{
+			return (Int32)Enum.Parse(e.GetType(), e.ToString());
+		}
+	}
 }
