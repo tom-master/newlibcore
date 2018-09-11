ca83829c50a24b39c559237ffa0291aef79b4682
zifan.hao <569192072@qq.com>
2018-09-11 14:38:53 +0800

    修改格式化sql时报错问题

--- NewLibCore.Data/Mapper/InternalDataStore/DataStore.cs
+++ NewLibCore.Data/Mapper/InternalDataStore/DataStore.cs
@@ -10,217 +10,203 @@ using NewLibCore.Data.Mapper.PropertyExtension;
 
 namespace NewLibCore.Data.Mapper.InternalDataStore
 {
-	public class DataStore : IDisposable
-	{
-		private DbConnection _connection;
+    public class DataStore : IDisposable
+    {
+        private DbConnection _connection;
 
-		private DbTransaction _dataTransaction;
+        private DbTransaction _dataTransaction;
 
-		private Boolean disposed = false;
+        private Boolean disposed = false;
 
-		private Boolean _useTransaction;
+        private Boolean _useTransaction;
 
-		private Boolean _noExecuteMode = false;
+        private Boolean _noExecuteMode = false;
 
-		public DataStore(String connection, Boolean noExecuteMode = false)
-		{
-			_connection = new MySqlConnection(connection);
-			_noExecuteMode = noExecuteMode;
-		}
+        public DataStore(String connection, Boolean noExecuteMode = false)
+        {
+            _connection = new MySqlConnection(connection);
+            _noExecuteMode = noExecuteMode;
+        }
 
-		public void OpenTransaction()
-		{
-			_useTransaction = true;
-		}
+        public void OpenTransaction()
+        {
+            _useTransaction = true;
+        }
 
-		private DbTransaction GetNonceTransaction()
-		{
-			if (_useTransaction)
-			{
-				if (_dataTransaction == null)
-				{
-					_useTransaction = true;
-					_dataTransaction = _connection.BeginTransaction();
-				}
-				return _dataTransaction;
-			}
-			throw new Exception("没有启动事务");
-		}
+        private DbTransaction GetNonceTransaction()
+        {
+            if (_useTransaction)
+            {
+                if (_dataTransaction == null)
+                {
+                    _useTransaction = true;
+                    _dataTransaction = _connection.BeginTransaction();
+                }
+                return _dataTransaction;
+            }
+            throw new Exception("没有启动事务");
+        }
 
-		public void Commit()
-		{
-			if (_useTransaction)
-			{
-				_dataTransaction.Commit();
-				return;
-			}
-			throw new Exception("没有启动事务，无法执行事务提交");
-		}
+        public void Commit()
+        {
+            if (_useTransaction)
+            {
+                _dataTransaction.Commit();
+                return;
+            }
+            throw new Exception("没有启动事务，无法执行事务提交");
+        }
 
-		public void Rollback()
-		{
-			if (_useTransaction)
-			{
-				_dataTransaction?.Rollback();
-				return;
-			}
-			throw new Exception("没有启动事务，无法执行事务回滚");
-		}
+        public void Rollback()
+        {
+            if (_useTransaction)
+            {
+                _dataTransaction?.Rollback();
+                return;
+            }
+            throw new Exception("没有启动事务，无法执行事务回滚");
+        }
 
-		private void Open()
-		{
-			if (_connection.State == ConnectionState.Closed)
-			{
-				_connection.Open();
-			}
-		}
+        private void Open()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
 
-		public Int32 Add<TModel>(TModel model) where TModel : PropertyMonitor, new()
-		{
-			SqlBuilder<TModel> builder = new AddBuilder<TModel>(model, true);
-			var entry = builder.Build();
-			return SqlExecute($@"{entry.ToString()}", entry.ParameterMappers, CommandType.Text);
-		}
+        public Int32 Add<TModel>(TModel model) where TModel : PropertyMonitor, new()
+        {
+            SqlBuilder<TModel> builder = new AddBuilder<TModel>(model, true);
+            var entry = builder.Build();
+            return SqlExecute($@"{entry.FormatSql()}", entry.ParameterMappers, CommandType.Text);
+        }
 
-		public Int32 Modify<TModel>(TModel model, Expression<Func<TModel, Boolean>> where = null) where TModel : PropertyMonitor, new()
-		{
-			SqlBuilder<TModel> builder = new ModifyBuilder<TModel>(model, where, true);
-			var entry = builder.Build();
-			return SqlExecute(entry.ToString(), entry.ParameterMappers, CommandType.Text, true);
-		}
+        public Int32 Modify<TModel>(TModel model, Expression<Func<TModel, Boolean>> where = null) where TModel : PropertyMonitor, new()
+        {
+            SqlBuilder<TModel> builder = new ModifyBuilder<TModel>(model, where, true);
+            var entry = builder.Build();
+            return SqlExecute(entry.FormatSql(), entry.ParameterMappers, CommandType.Text, true);
+        }
 
-		public List<TModel> Find<TModel>(Expression<Func<TModel, Boolean>> where, Expression<Func<TModel, Object>> fields) where TModel : PropertyMonitor, new()
-		{
-			var builder = new SelectBuilder<TModel>(null, where, fields);
-			var entry = builder.Build();
-			return Find<TModel>(entry.FormatSql(), entry.ParameterMappers, CommandType.Text);
-		}
+        public TModel FindOne<TModel>(String sqlStr, IEnumerable<ParameterMapper> parameters = null, CommandType commandType = CommandType.Text) where TModel : class, new()
+        {
+            return Find<TModel>(sqlStr, parameters, commandType).FirstOrDefault();
+        }
 
-		public TModel FindOne<TModel>(Expression<Func<TModel, Boolean>> where, Expression<Func<TModel, Object>> fields) where TModel : PropertyMonitor, new()
-		{
-			var builder = new SelectBuilder<TModel>(null, where, fields);
-			var entry = builder.Build();
-			return Find<TModel>(entry.FormatSql(), entry.ParameterMappers, CommandType.Text).FirstOrDefault();
-		}
+        public List<TModel> Find<TModel>(String sqlStr, IEnumerable<ParameterMapper> parameters = null, CommandType commandType = CommandType.Text) where TModel : class, new()
+        {
+            Open();
+            using (DbCommand cmd = _connection.CreateCommand())
+            {
+                if (_useTransaction)
+                {
+                    cmd.Transaction = GetNonceTransaction();
+                }
+                cmd.CommandType = commandType;
+                cmd.CommandText = sqlStr;
+                if (parameters != null && parameters.Any())
+                {
+                    cmd.Parameters.AddRange(parameters.Select(s => (DbParameter)s).ToArray());
+                }
 
-		public TModel FindOne<TModel>(String sqlStr, IEnumerable<ParameterMapper> parameters = null, CommandType commandType = CommandType.Text) where TModel : class, new()
-		{
-			return Find<TModel>(sqlStr, parameters, commandType).FirstOrDefault();
-		}
+                var dr = cmd.ExecuteReader();
+                var tmpDt = new DataTable("tmpDt");
+                tmpDt.Load(dr, LoadOption.Upsert);
+                dr.Close();
+                return tmpDt.AsList<TModel>().ToList();
+            }
+        }
 
-		public List<TModel> Find<TModel>(String sqlStr, IEnumerable<ParameterMapper> parameters = null, CommandType commandType = CommandType.Text) where TModel : class, new()
-		{
-			Open();
-			using (DbCommand cmd = _connection.CreateCommand())
-			{
-				if (_useTransaction)
-				{
-					cmd.Transaction = GetNonceTransaction();
-				}
-				cmd.CommandType = commandType;
-				cmd.CommandText = sqlStr;
-				if (parameters != null && parameters.Any())
-				{
-					cmd.Parameters.AddRange(parameters.Select(s => (DbParameter)s).ToArray());
-				}
+        public TValue FindSingleValue<TValue>(String sqlStr, IEnumerable<ParameterMapper> parameters = null, CommandType commandType = CommandType.Text)
+        {
+            Open();
+            try
+            {
+                using (DbCommand cmd = _connection.CreateCommand())
+                {
+                    if (_useTransaction)
+                    {
+                        cmd.Transaction = GetNonceTransaction();
+                    }
+                    cmd.CommandType = commandType;
+                    cmd.CommandText = sqlStr;
+                    //参数化
+                    if (parameters != null && parameters.Any())
+                    {
+                        cmd.Parameters.AddRange(parameters.Select(s => (DbParameter)s).ToArray());
+                    }
 
-				var dr = cmd.ExecuteReader();
-				var tmpDt = new DataTable("tmpDt");
-				tmpDt.Load(dr, LoadOption.Upsert);
-				dr.Close();
-				return tmpDt.AsList<TModel>().ToList();
-			}
-		}
+                    TValue obj = (TValue)Convert.ChangeType(cmd.ExecuteScalar(), typeof(TValue));
+                    cmd.Parameters.Clear();
+                    return obj;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
 
-		public TValue FindSingleValue<TValue>(String sqlStr, IEnumerable<ParameterMapper> parameters = null, CommandType commandType = CommandType.Text)
-		{
-			Open();
-			try
-			{
-				using (DbCommand cmd = _connection.CreateCommand())
-				{
-					if (_useTransaction)
-					{
-						cmd.Transaction = GetNonceTransaction();
-					}
-					cmd.CommandType = commandType;
-					cmd.CommandText = sqlStr;
-					//参数化
-					if (parameters != null && parameters.Any())
-					{
-						cmd.Parameters.AddRange(parameters.Select(s => (DbParameter)s).ToArray());
-					}
+        private Int32 SqlExecute(String sqlStr, IEnumerable<ParameterMapper> parameters = null, CommandType commandType = CommandType.Text, Boolean isModify = false)
+        {
+            Open();
+            using (DbCommand cmd = _connection.CreateCommand())
+            {
+                if (_useTransaction)
+                {
+                    cmd.Transaction = GetNonceTransaction();
+                }
+                cmd.CommandType = commandType;
+                cmd.CommandText = sqlStr;
+                if (parameters != null && parameters.Any())
+                {
+                    cmd.Parameters.AddRange(parameters.Select(s => (DbParameter)s).ToArray());
+                }
+                Int32 count = 0;
+                if (!isModify)
+                {
+                    count = Int32.Parse(cmd.ExecuteScalar().ToString());
+                }
+                else
+                {
+                    count = Int32.Parse(cmd.ExecuteNonQuery().ToString());
+                }
+                cmd.Parameters.Clear();
+                return count;
+            }
+        }
 
-					TValue obj = (TValue)Convert.ChangeType(cmd.ExecuteScalar(), typeof(TValue));
-					cmd.Parameters.Clear();
-					return obj;
-				}
-			}
-			catch (Exception)
-			{
-				throw;
-			}
-		}
+        #region dispose
 
-		private Int32 SqlExecute(String sqlStr, IEnumerable<ParameterMapper> parameters = null, CommandType commandType = CommandType.Text, Boolean isModify = false)
-		{
-			Open();
-			using (DbCommand cmd = _connection.CreateCommand())
-			{
-				if (_useTransaction)
-				{
-					cmd.Transaction = GetNonceTransaction();
-				}
-				cmd.CommandType = commandType;
-				cmd.CommandText = sqlStr;
-				if (parameters != null && parameters.Any())
-				{
-					cmd.Parameters.AddRange(parameters.Select(s => (DbParameter)s).ToArray());
-				}
-				Int32 count = 0;
-				if (!isModify)
-				{
-					count = Int32.Parse(cmd.ExecuteScalar().ToString());
-				}
-				else
-				{
-					count = Int32.Parse(cmd.ExecuteNonQuery().ToString());
-				}
-				cmd.Parameters.Clear();
-				return count;
-			}
-		}
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-		#region 
+        private void Dispose(Boolean disposing)
+        {
+            if (!disposed)
+            {
+                if (!disposing)
+                {
+                    return;
+                }
 
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+                if (_connection != null)
+                {
+                    if (_connection.State != ConnectionState.Closed)
+                    {
+                        _connection.Close();
+                    }
+                    _connection.Dispose();
+                    _connection = null;
+                }
+                disposed = true;
+            }
+        }
 
-		private void Dispose(Boolean disposing)
-		{
-			if (!disposed)
-			{
-				if (!disposing)
-				{
-					return;
-				}
-
-				if (_connection != null)
-				{
-					if (_connection.State != ConnectionState.Closed)
-					{
-						_connection.Close();
-					}
-					_connection.Dispose();
-					_connection = null;
-				}
-				disposed = true;
-			}
-		}
-
-		#endregion
-	}
+        #endregion
+    }
 }
