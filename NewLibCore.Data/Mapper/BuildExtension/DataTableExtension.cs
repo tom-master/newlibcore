cc79f305369a13ec44aeb630c31246c1009bb75b
haoxiaofan <569192072@qq.com>
2018-11-15 22:23:40 +0800

    format code

--- NewLibCore.Data/Mapper/BuildExtension/DataTableExtension.cs
+++ NewLibCore.Data/Mapper/BuildExtension/DataTableExtension.cs
@@ -89,19 +89,14 @@ namespace NewLibCore.Data.Mapper.DataExtension
             var value = Expression.Parameter(typeof(object), "value");
 
             UnaryExpression instanceCast = (!this.Property.DeclaringType.IsValueType) ? Expression.TypeAs(instance, this.Property.DeclaringType) : Expression.Convert(instance, this.Property.DeclaringType);
-
             UnaryExpression valueCast = (!this.Property.PropertyType.IsValueType) ? Expression.TypeAs(value, this.Property.PropertyType) : Expression.Convert(value, this.Property.PropertyType);
-
             this.SetDelegate = Expression.Lambda<Action<object, object>>(Expression.Call(instanceCast, this.Property.SetMethod, valueCast), new ParameterExpression[] { instance, value }).Compile();
-
         }
 
         private void InitializeGet()
         {
             var instance = Expression.Parameter(typeof(object), "instance");
-
             UnaryExpression instanceCast = (!this.Property.DeclaringType.IsValueType) ? Expression.TypeAs(instance, this.Property.DeclaringType) : Expression.Convert(instance, this.Property.DeclaringType);
-
             this.GetDelegate = Expression.Lambda<Func<object, object>>(Expression.TypeAs(Expression.Call(instanceCast, this.Property.GetGetMethod()), typeof(object)), instance).Compile();
         }
 
