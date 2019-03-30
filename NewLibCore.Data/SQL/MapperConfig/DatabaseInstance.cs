27a4fe7e29e7c5e72dc508e52e88f11e132549fb
haoxiaofan <569192072@qq.com>
2019-03-30 17:15:13 +0800

    add commit

---
+++ NewLibCore.Data/SQL/MapperConfig/DatabaseInstance.cs
@@ -0,0 +1,150 @@
+﻿using MySql.Data.MySqlClient;
+using NewLibCore.Data.SQL.MapperExtension;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace NewLibCore.Data.SQL.MapperConfig
+{
+    internal abstract class DatabaseInstance
+    {
+        protected static IDictionary<RelationType, String> RelationMapper = new Dictionary<RelationType, String>();
+
+        protected static IDictionary<JoinType, String> JoinTypeMapper = new Dictionary<JoinType, String>();
+
+        protected static IDictionary<OrderByType, String> OrderTypeMapper = new Dictionary<OrderByType, String>();
+
+        protected String ConnectionString { get { return Host.GetHostVar("database"); } }
+
+        protected DatabaseInstance()
+        {
+            InitRelationType();
+            InitJoinType();
+            InitOrderType();
+        }
+
+        protected virtual void AppendRelationType() { }
+
+        internal virtual StatementExtension GetStatementExtension() { return null; }
+
+        internal abstract DbConnection GetConnectionInstance();
+
+        internal abstract DbParameter GetParameterInstance();
+
+        internal abstract String RelationBuilder(RelationType relationType, String left, String right);
+
+        internal String JoinBuilder(JoinType joinType, String left, String right)
+        {
+            return String.Format(JoinTypeMapper[joinType], left, right);
+        }
+
+        internal String OrderByBuilder(OrderByType orderByType, String left, String right)
+        {
+            return String.Format(OrderTypeMapper[orderByType], left, right);
+        }
+
+        private void InitRelationType()
+        {
+            RelationMapper.Add(RelationType.AND, "{0} AND {1}");
+            RelationMapper.Add(RelationType.OR, "{0} OR {1}");
+            RelationMapper.Add(RelationType.IN, "{0} IN ({1})");
+            RelationMapper.Add(RelationType.EQ, "{0} = {1}");
+            RelationMapper.Add(RelationType.NQ, "{0} <> {1}");
+            RelationMapper.Add(RelationType.GT, "{0} < {1}");
+            RelationMapper.Add(RelationType.LT, "{0} > {1}");
+            RelationMapper.Add(RelationType.GE, "{0} <= {1}");
+            RelationMapper.Add(RelationType.LE, "{0} >= {1}");
+        }
+
+        private void InitJoinType()
+        {
+            JoinTypeMapper.Add(JoinType.NONE, "");
+            JoinTypeMapper.Add(JoinType.INNER, " INNER JOIN {0} AS {1} ON ");
+            JoinTypeMapper.Add(JoinType.LEFT, " LEFT JOIN {0} AS {1} ON ");
+            JoinTypeMapper.Add(JoinType.RIGHT, "{0} RIGHT JOIN {1}");
+        }
+
+        private void InitOrderType()
+        {
+            OrderTypeMapper.Add(OrderByType.ASC, " ORDER BY {0} ASC ");
+            OrderTypeMapper.Add(OrderByType.DESC, " ORDER BY {0} DESC ");
+        }
+    }
+
+    internal class MsSqlInstance : DatabaseInstance
+    {
+        protected override void AppendRelationType()
+        {
+            RelationMapper.Add(RelationType.FULL_LIKE, "{0} LIKE '%{1}%'");
+            RelationMapper.Add(RelationType.START_LIKE, "{0} LIKE '{1}%'");
+            RelationMapper.Add(RelationType.END_LIKE, "{0} LIKE '%{1}' ");
+        }
+
+        internal override DbConnection GetConnectionInstance()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+        internal override DbParameter GetParameterInstance()
+        {
+            return new SqlParameter();
+        }
+        internal override String RelationBuilder(RelationType relationType, String left, String right)
+        {
+            return String.Format(RelationMapper[relationType], left, right);
+        }
+
+        internal override StatementExtension GetStatementExtension()
+        {
+            return new StatementExtension
+            {
+                Identity = " SELECT @@IDENTITY",
+                RowCount = " SELECT @@ROWCOUNT",
+                Page = " OFFSET ({value}) ROWS FETCH NEXT {pageSize} ROWS ONLY"
+            };
+        }
+    }
+
+    internal class MySqlInstance : DatabaseInstance
+    {
+        protected override void AppendRelationType()
+        {
+            RelationMapper.Add(RelationType.FULL_LIKE, "{0} LIKE CONCAT('%',{1},'%')");
+            RelationMapper.Add(RelationType.START_LIKE, "{0} LIKE CONCAT('',{1},'%')");
+            RelationMapper.Add(RelationType.END_LIKE, "{0} LIKE CONCAT('%',{1},'')");
+        }
+
+        internal override DbConnection GetConnectionInstance()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+        internal override DbParameter GetParameterInstance()
+        {
+            return new MySqlParameter();
+        }
+        internal override String RelationBuilder(RelationType relationType, String left, String right)
+        {
+            return String.Format(RelationMapper[relationType], left, right);
+        }
+
+        internal override StatementExtension GetStatementExtension()
+        {
+            return new StatementExtension
+            {
+                Identity = " ; SELECT CAST(@@IDENTITY AS SIGNED) AS c ",
+                RowCount = " ; SELECT CAST(ROW_COUNT() AS SIGNED) AS c",
+                Page = " LIMIT {value},{pageSize}"
+            };
+        }
+    }
+
+    internal class StatementExtension
+    {
+        public String Identity { get; internal set; }
+
+        public String RowCount { get; internal set; }
+
+        public String Page { get; internal set; }
+    }
+}
--- NewLibCore.Data/SQL/MapperExtension/EnumType.cs
+++ NewLibCore.Data/SQL/MapperExtension/EnumType.cs
@@ -5,82 +5,45 @@ namespace NewLibCore.Data.SQL.MapperExtension
     public enum ExecuteType
     {
         SELECT = 1,
-
         UPDATE = 2,
-
         INSERT = 3,
-
         SELECT_SINGLE = 4
     }
 
     public enum DatabaseType
     {
         NONE = 0,
-
         MSSQL = 1,
-
         MYSQL = 2
     }
 
     public enum OrderByType
     {
-        [Description(" ORDER BY {0} ASC ")]
         ASC = 1,
-
-        [Description(" ORDER BY {0} DESC ")]
         DESC = 2
     }
 
     public enum JoinType
     {
         NONE = 0,
-
-        [Description(" INNER JOIN {0} AS {1} ON ")]
         INNER = 1,
-
-        [Description(" LEFT JOIN {0} AS {1} ON ")]
         LEFT = 2,
-
-        [Description("{0} RIGHT JOIN {1}")]
         RIGHT = 3
     }
 
     internal enum RelationType
     {
-        [Description("{0} AND {1}")]
         AND = 1,
-
-        [Description("{0} OR {1}")]
         OR = 2,
-
-        [Description("{0} LIKE '%{1}%'")]
         FULL_LIKE = 3,
-
-        [Description("{0} LIKE '{1}%'")]
         START_LIKE = 4,
-
-        [Description("{0} LIKE '%{1}'")]
         END_LIKE = 5,
-
-        [Description("{0} IN ({1})")]
         IN = 6,
-
-        [Description("{0} = {1}")]
         EQ = 7,
-
-        [Description("{0} <> {1}")]
         NQ = 8,
-
-        [Description("{0} < {1}")]
         GT = 9,
-
-        [Description("{0} > {1}")]
         LT = 10,
-
-        [Description("{0} <= {1}")]
         GE = 11,
-
-        [Description("{0} >= {1}")]
         LE = 12
     }
 }
