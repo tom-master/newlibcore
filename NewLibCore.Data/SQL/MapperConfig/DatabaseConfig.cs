910cdf8b1d2426bacd3ac849568d642704f9fd0e
haoxiaofan <569192072@qq.com>
2019-03-30 17:24:04 +0800

    add commit

--- NewLibCore.Data/SQL/MapperConfig/DatabaseConfig.cs
+++ NewLibCore.Data/SQL/MapperConfig/DatabaseConfig.cs
@@ -8,18 +8,9 @@ namespace NewLibCore.Data.SQL.MapperConfig
 {
     public class DatabaseConfig
     {
-        internal static String ConnectionString { get { return Host.GetHostVar("database"); } }
-
-        public static DatabaseType Type { get; private set; }
- 
-        internal static DatabaseSyntaxBuilder DatabaseSyntax { get; private set; }
+        internal static DatabaseInstance Instance { get; private set; }
 
         internal static ILogger Logger { get; private set; }
-
-        static DatabaseConfig()
-        {
-            Type = DatabaseType.NONE;
-        }
 
         public static void InitLogger(ILogger logger = null)
         {
@@ -36,69 +27,23 @@ namespace NewLibCore.Data.SQL.MapperConfig
             SwitchTo(DatabaseType.MSSQL);
         }
 
-        internal static DbConnection GetConnectionInstance()
-        {
-            var connection = Host.GetHostVar("database");
-
-            switch (Type)
-            {
-                case DatabaseType.MSSQL:
-                {
-                    return new SqlConnection(connection);
-                }
-                case DatabaseType.MYSQL:
-                {
-                    return new MySqlConnection(connection);
-                }
-                default:
-                {
-                    throw new ArgumentException($@"暂不支持的数据库类型:{Type}");
-                }
-            }
-        }
-
-        internal static DbParameter GetDbParameterInstance()
-        {
-            switch (Type)
-            {
-                case DatabaseType.MSSQL:
-                    return new SqlParameter();
-                case DatabaseType.MYSQL:
-                    return new MySqlParameter();
-                default:
-                    throw new ArgumentException($@"暂不支持的数据库类型:{DatabaseConfig.Type}");
-            }
-        }
-
         private static void SwitchTo(DatabaseType database)
         {
             switch (database)
             {
                 case DatabaseType.MSSQL:
                 {
-                    DatabaseSyntax = new MsSqlSyntaxBuilder
-                    {
-                        IdentitySuffix = " SELECT @@IDENTITY",
-                        RowCountSuffix = " SELECT @@ROWCOUNT",
-                        Page = " OFFSET ({value}) ROWS FETCH NEXT {pageSize} ROWS ONLY"
-                    };
+                    Instance = new MsSqlInstance();
                     break;
                 }
                 case DatabaseType.MYSQL:
                 {
-                    DatabaseSyntax = new MySqlSyntaxBuilder
-                    {
-                        IdentitySuffix = " ; SELECT CAST(@@IDENTITY AS SIGNED) AS c ",
-                        RowCountSuffix = " ; SELECT CAST(ROW_COUNT() AS SIGNED) AS c",
-                        Page = " LIMIT {value},{pageSize}"
-                    };
+                    Instance = new MsSqlInstance();
                     break;
                 }
                 default:
                     break;
             }
-
-            Type = database;
         }
     }
 
