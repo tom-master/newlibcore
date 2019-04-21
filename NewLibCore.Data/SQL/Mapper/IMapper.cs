a9c67b0e273af4bba7b66f83520cc2bedfc6e0ce
haoxiaofan <569192072@qq.com>
2019-04-21 10:51:05 +0800

    add commit

--- NewLibCore.Data/SQL/Mapper/IMapper.cs
+++ NewLibCore.Data/SQL/Mapper/IMapper.cs
@@ -54,7 +54,7 @@ namespace NewLibCore.Data.SQL.Mapper
     }
   
     public interface ISqlExecutor
-    {
+    { 
         List<TModel> ToList<TModel>(String sql, IEnumerable<EntityParameter> parameters = null) where TModel : new();
 
         TModel ToSingle<TModel>(String sql, IEnumerable<EntityParameter> parameters = null) where TModel : new();
