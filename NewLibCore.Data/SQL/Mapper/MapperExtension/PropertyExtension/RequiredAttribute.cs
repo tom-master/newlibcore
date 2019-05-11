66fb90a8dcf4b71589c4b48691ce7de83e3b7efc
haoxiaofan <569192072@qq.com>
2019-05-11 21:00:02 +0800

    add commit

--- NewLibCore.Data/SQL/Mapper/MapperExtension/PropertyExtension/RequiredAttribute.cs
+++ NewLibCore.Data/SQL/Mapper/MapperExtension/PropertyExtension/RequiredAttribute.cs
@@ -2,37 +2,21 @@
 
 namespace NewLibCore.Data.SQL.Mapper.Extension.PropertyExtension
 {
-    public class RequiredAttribute : PropertyValidate
-    {
-        public override Int32 Order
-        {
-            get { return 3; }
-        }
+	public class RequiredAttribute : PropertyValidate
+	{
+		public override Int32 Order
+		{
+			get { return 3; }
+		}
 
+		public override String FailReason(String fieldName)
+		{
+			return $@"{fieldName} 为必填项!";
+		}
 
-        public override String FailReason(String fieldName)
-        {
-            return $@"{fieldName} 为必填项!";
-        }
-
-        public override Boolean IsValidate(Object value)
-        {
-            return !String.IsNullOrEmpty(value + "");
-            //if (String.IsNullOrEmpty(value + ""))
-            //{
-            //    return false;
-            //}
-
-            //var isComplexType = TypeDescriptor.GetConverter(value.GetType()).CanConvertFrom(typeof(String));
-            //if (!isComplexType)
-            //{
-            //    var objType = value.GetType();
-            //    if (objType.IsArray || objType.GetGenericTypeDefinition() == typeof(IList<>))
-            //    {
-            //        return !(((IList)value).Count == 0);
-            //    }
-            //}
-            //return true;
-        }
-    }
+		public override Boolean IsValidate(Object value)
+		{
+			return !String.IsNullOrEmpty(value + "");
+		}
+	}
 }
