f12fce726adf4fa09c5482ad089199b55d1e5dc0
haoxiaofan <569192072@qq.com>
2019-05-26 19:44:01 +0800

    add commit

---
+++ NewLibCore.Data/SQL/Mapper/MapperExtension/TypeExtension.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using NewLibCore.Data.SQL.Mapper.Extension.PropertyExtension;
+
+namespace NewLibCore.Data.SQL.Mapper.Extension
+{
+    public static class TypeExtension
+    {
+        public static String GetAliasName(this Type t)
+        {
+
+            var attrubutes = t.GetCustomAttributes(typeof(TableNameAttribute), true);
+            if (!attrubutes.Any())
+            {
+                return t.Name;
+            }
+
+            return ((TableNameAttribute)attrubutes.FirstOrDefault()).TableName;
+        }
+    }
+}
