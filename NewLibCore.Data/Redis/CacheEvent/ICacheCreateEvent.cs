8fbe83d5a40e26d71d7cc2aeb8b1f3c9ae776d2b
haoxiaofan <569192072@qq.com>
2019-05-27 10:15:06 +0800

    add commit

--- NewLibCore.Data/Redis/CacheEvent/ICacheCreateEvent.cs
+++ NewLibCore.Data/Redis/CacheEvent/ICacheCreateEvent.cs
@@ -5,7 +5,7 @@ namespace NewLibCore.Data.Redis.CacheEvent
     public abstract class CacheEvent
     {
         protected abstract event EventHandler OnCreate;
-        
+          
         protected abstract event EventHandler OnModify;
 
     }
