65a8e7c3aff3296b0a12d5534d903435a174d586
haoxiaofan <569192072@qq.com>
2019-07-11 14:13:51 +0800

    add commit

--- NewLibCore.Data/SQL/Mapper/MapperHandler/InsertHandler.cs
+++ NewLibCore.Data/SQL/Mapper/MapperHandler/InsertHandler.cs
@@ -37,7 +37,7 @@ namespace NewLibCore.Data.SQL.Mapper
 
             var tableName = typeof(TModel).GetTableName().TableName;
             var template = ReplacePlaceholder(propertyInfos, tableName);
-            return TranslateResult.CreateResult().Append(template, propertyInfos.Select(c => new EntityParameter(c.Key, c.Value))).Execute();
+            return TranslateResult.CreateResult(null).Append(template, propertyInfos.Select(c => new EntityParameter(c.Key, c.Value))).Execute();
         }
 
 
