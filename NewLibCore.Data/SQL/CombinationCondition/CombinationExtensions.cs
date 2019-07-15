b332471fd5f8f128ed94ae8d61a6ccb827419d45
haoxiaofan <569192072@qq.com>
2019-07-15 20:24:01 +0800

    add commit

--- NewLibCore.Data/SQL/CombinationCondition/CombinationExtensions.cs
+++ NewLibCore.Data/SQL/CombinationCondition/CombinationExtensions.cs
@@ -4,10 +4,18 @@ using NewLibCore.Data.SQL.Mapper.Extension;
 
 namespace NewLibCore.Data.SQL.CombinationCondition
 {
-
+    /// <summary>
+    /// 规约扩展
+    /// </summary>
     public static class CombinationExtensions
     {
 
+        /// <summary>
+        /// 表示一个 与 规约
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <typeparam name="T"></typeparam>
         public static void And<T>(this Combination<T> left, Expression<Func<T, Boolean>> right) where T : EntityBase
         {
             if (left.CombinationExpression == default)
@@ -25,6 +33,12 @@ namespace NewLibCore.Data.SQL.CombinationCondition
             left.CombinationExpression = Expression.Lambda<Func<T, Boolean>>(newExpression, internalParameter);
         }
 
+        /// <summary>
+        /// 表示一个 或 规约
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <typeparam name="T"></typeparam>
         public static void Or<T>(this Combination<T> left, Expression<Func<T, Boolean>> right) where T : EntityBase
         {
 
@@ -43,6 +57,11 @@ namespace NewLibCore.Data.SQL.CombinationCondition
             left.CombinationExpression = Expression.Lambda<Func<T, Boolean>>(orExpression, internalParameter);
         }
 
+        /// <summary>
+        /// 表示一个 非 规约
+        /// </summary>
+        /// <param name="left"></param>
+        /// <typeparam name="T"></typeparam>
         public static void Not<T>(this Combination<T> left) where T : EntityBase
         {
             var lambdaExpression = (LambdaExpression)left.CombinationExpression;
