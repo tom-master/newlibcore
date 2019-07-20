b4d9eb4a0a987e381b3079434a9c4a02ee777b4b
haoxiaofan <569192072@qq.com>
2019-07-20 11:38:13 +0800

    add commit

--- NewLibCore.Data/SQL/Mapper/EntityMapper.cs
+++ NewLibCore.Data/SQL/Mapper/EntityMapper.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using NewLibCore.Data.SQL.Mapper.EntityExtension;
-using NewLibCore.Data.SQL.Mapper.Execute; 
+using NewLibCore.Data.SQL.Mapper.Execute;
 using NewLibCore.Data.SQL.Mapper.OperationProvider;
 using NewLibCore.Data.SQL.Mapper.OperationProvider.Imp;
 using NewLibCore.Validate;
@@ -18,12 +18,25 @@ namespace NewLibCore.Data.SQL.Mapper
             _executeCore = new ExecuteCore();
         }
 
+        /// <summary>
+        /// 添加一個TModel
+        /// </summary>
+        /// <param name="model"></param>
+        /// <typeparam name="TModel"></typeparam>
+        /// <returns></returns>
         public TModel Add<TModel>(TModel model) where TModel : EntityBase, new()
         {
             Parameter.Validate(model);
             return new AddMapper<TModel>(_executeCore).Add(model);
         }
 
+        /// <summary>
+        /// 修改一個TModel
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="expression"></param>
+        /// <typeparam name="TModel"></typeparam>
+        /// <returns></returns>
         public Boolean Modify<TModel>(TModel model, Expression<Func<TModel, Boolean>> expression) where TModel : EntityBase, new()
         {
             Parameter.Validate(model);
@@ -31,39 +44,74 @@ namespace NewLibCore.Data.SQL.Mapper
             return new ModifyMapper<TModel>(_executeCore).Update(model, expression);
         }
 
+        /// <summary>
+        /// 查询一個TModel
+        /// </summary>
+        /// <param name="fields"></param>
+        /// <typeparam name="TModel"></typeparam>
+        /// <returns></returns>
         public ISearchMapper<TModel> Select<TModel>(Expression<Func<TModel, dynamic>> fields = null) where TModel : EntityBase, new()
         {
             return new SearchMapper<TModel>(_executeCore).Select(fields);
         }
 
+        /// <summary>
+        /// 查詢一個TModel
+        /// </summary>
+        /// <param name="fields"></param>
+        /// <typeparam name="TModel"></typeparam>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public ISearchMapper<TModel> Select<TModel, T>(Expression<Func<TModel, T, dynamic>> fields = null) where TModel : EntityBase, new()
         where T : EntityBase, new()
         {
             return new SearchMapper<TModel>(_executeCore).Select(fields);
         }
 
+        /// <summary>
+        /// 执行一個返回列表的sql语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <typeparam name="TModel"></typeparam>
+        /// <returns></returns>
         public List<TModel> ExecuteToList<TModel>(String sql, IEnumerable<EntityParameter> parameters = null) where TModel : new()
         {
             Parameter.Validate(sql);
             return new RawExecutor(_executeCore).ToList<TModel>(sql, parameters);
         }
 
+        /// <summary>
+        /// 执行一个返回单个TModel的sql语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <typeparam name="TModel"></typeparam>
+        /// <returns></returns>
         public TModel ExecuteToSingle<TModel>(String sql, IEnumerable<EntityParameter> parameters = null) where TModel : new()
         {
             Parameter.Validate(sql);
             return new RawExecutor(_executeCore).ToSingle<TModel>(sql, parameters);
         }
 
+        /// <summary>
+        /// 开启事物
+        /// </summary>
         public void OpenTransaction()
         {
             _executeCore.OpenTransaction();
         }
-
+        /// <summary>
+        /// 提交事物
+        /// </summary>
         public void Commit()
         {
             _executeCore.Commit();
         }
 
+        /// <summary>
+        /// 回滚事物
+        /// </summary>
         public void Rollback()
         {
             _executeCore.Rollback();
