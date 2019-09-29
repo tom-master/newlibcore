cbdd2f5743cc9b52cb015538b6df11b8400b8e85
haoxiaofan <569192072@qq.com>
2019-09-29 10:43:29 +0800

    add commit

--- NewLibCore.Data/SQL/Mapper/ExpressionStatment/Statement.cs
+++ NewLibCore.Data/SQL/Mapper/ExpressionStatment/Statement.cs
@@ -13,4 +13,41 @@ namespace NewLibCore.Data.SQL.Mapper.ExpressionStatment
 
         protected internal IReadOnlyList<KeyValuePair<String, String>> AliaNameMapper { get; set; }
     }
+
+    /// <summary>
+    /// 连接语句对象
+    /// </summary>
+    internal class JoinExpressionMapper : Statement
+    {
+        protected internal String MainTable { get; set; }
+
+        protected internal JoinRelation JoinRelation { get; set; }
+    }
+
+
+    /// <summary>
+    /// 排序语句对象
+    /// </summary>
+    internal class OrderExpressionMapper : Statement
+    {
+        protected internal OrderByType OrderBy { get; set; }
+    }
+
+    /// <summary>
+    /// 分页语句对象
+    /// </summary>
+    internal class PaginationExpressionMapper : Statement
+    {
+        internal Int32 Index { get; set; }
+
+        internal Int32 Size { get; set; }
+    }
+
+    /// <summary>
+    /// 简单语句对象
+    /// </summary>
+    internal class SimpleExpressionMapper : Statement
+    {
+
+    }
 }
