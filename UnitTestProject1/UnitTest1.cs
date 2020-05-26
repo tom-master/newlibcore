df2555c568faab669fbc7e73028311c5157af5f0
haozifan <huakeba@outlook.com>
2020-05-26 14:46:34 +0800

    add commit

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,28 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.IO;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NewLibCore;
-using NewLibCore.Data.SQL.DataExtension;
-using NewLibCore.Data.SQL.InternalDataStore;
-using NewLibCore.Data.SQL.MapperExtension;
-using NewLibCore.Data.SQL.PropertyExtension;
-
 
 namespace UnitTestProject1
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-           
-        }
-    }
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
 }
