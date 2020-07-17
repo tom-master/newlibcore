814073a2fe92b88e2bc6b2a6c2469f9b2cf6d9a3
xiaofan <569192072@qq.com>
2020-07-17 13:13:07 +0800

    add commit

--- NewLibCore.UnitTest/UnitTest1.cs
+++ NewLibCore.UnitTest/UnitTest1.cs
@@ -14,49 +14,21 @@ namespace NewLibCore.UnitTest
     [TestClass]
     public class UnitTest1
     {
-        private static readonly Object _obj = new object();
         [TestMethod]
         public void TestMethod1()
         {
-            // ICacheQueryProvider cacheProvider = new DefaultRedisQueryProvider(0, "39.106.106.137:9736,allowAdmin=false,password=UGieaY5u9SwRUzoxbuePq,connectTimeout=15000,keepAlive=60,syncTimeout=3000");
-            // var rr = cacheProvider.StringGet<Config>("NewCrm:Config:UserId:4");
-            // cacheProvider.StringSet("NewCrm:Config:UserId:4", new Config
-            // {
-            //     Skin = "wasd123"
-            // });
-            EntityMapperConfig.InitDefaultSetting();
-            EntityMapperConfig.ConnectionStringName = "NewCrmDatabase";
-
-            using (var mapper = EntityMapper.CreateMapper())
-            {
-                var result = mapper.Query<User>().InnerJoin<Config>((a, b) => a.Id == b.UserId).Where(w => w.Id == 4).ToList();
-            }
-            // try
-            // {
-            //     Thread[] threads = new Thread[Environment.ProcessorCount];
-            //     for (int i = 0; i < Environment.ProcessorCount; i++)
-            //     {
-            //         threads[i] = new Thread(() =>
-            //         {
-            //             using (var mapper = EntityMapper.CreateMapper())
-            //             {
-            //                 var result = mapper.Query<User>().Where(w => w.Id == 4).ToList();
-            //                 Console.WriteLine(JsonSerializer.Serialize(result));
-            //             }
-            //         });
-            //     }
-
-            //     foreach (var item in threads)
-            //     {
-            //         item.Start();
-            //     }
-
-            // }
-            // catch (Exception)
-            // {
-
-            //     throw;
-            // }
+            var result1 = EnumExtensions.ToEnum<Test>(1);
+            var result2 = EnumExtensions.ToEnum<Test>("B");
+            var result3 = Test.A.ToInt32();
+            var result4 = Test.B.ToInt32();
+            var result5 = Test.C.ToInt32();
         }
+    }
+
+    public enum Test
+    {
+        A = 1,
+        B = 2,
+        C = 3
     }
 }
