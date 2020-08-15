a2b6e7e3c0b9f057b490c6d7323713b3b91db6ad
xiaofan <569192072@qq.com>
2020-08-15 15:36:50 +0800

    add commit

--- NewLibCore.Storage/SQL/EMapper/Component/Cache/DefaultQueryCache.cs
+++ NewLibCore.Storage/SQL/EMapper/Component/Cache/DefaultQueryCache.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.Caching;
 
-namespace NewLibCore.Data.SQL.Component.Cache
+namespace NewLibCore.Storage.SQL.Component.Cache
 {
     /// <summary>
     /// 提供对sql查询结果的缓存操作
