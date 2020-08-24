5aa988fb8e3a38f0a5b4af04fde02c29c45cce64
xiaofan <569192072@qq.com>
2020-08-24 15:42:53 +0800

    add commit

--- NewLibCore.Storage/SQL/EMapper/Extension/DataConvert/IConverter.cs
+++ NewLibCore.Storage/SQL/EMapper/Extension/DataConvert/IConverter.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Data;
 
-namespace NewLibCore.Data.SQL.DataConvert
+namespace NewLibCore.Storage.SQL.DataConvert
 {
     internal interface IConverter
     {
