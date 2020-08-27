4143ea4cc8a0e3fd0e277cd63eb9dd175ce7cb12
xiaofan <569192072@qq.com>
2020-08-27 15:43:55 +0800

    add commit

--- NewLibCore.Storage/SQL/EMapper/Extension/DataConvert/TupleConverter.cs
+++ NewLibCore.Storage/SQL/EMapper/Extension/DataConvert/TupleConverter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
-namespace NewLibCore.Data.SQL.DataConvert
+namespace NewLibCore.Storage.SQL.DataConvert
 {
     internal class TupleConverter : IConverter
     {
