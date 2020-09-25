3e7abd2053b33b6953ed53629f74fc33f2aa7ac8
xiaofan <569192072@qq.com>
2020-09-25 15:59:51 +0800

    add commit

--- NewLibCore.UnitTest/Entitys/System/AppStar.cs
+++ NewLibCore.UnitTest/Entitys/System/AppStar.cs
@@ -3,7 +3,7 @@ using NewLibCore.Storage.SQL;
 using NewLibCore.Storage.SQL.Validate;
 
 namespace NewLibCore.UnitTest.Entitys.System
-{`
+{
     [TableName("newcrm_app_star")]
     public partial class AppStar : EntityBase
     {
