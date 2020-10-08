8644f8cb29d0698cf55e923cbed5a08f0c3f2e6b
xiaofan <569192072@qq.com>
2020-10-08 13:23:55 +0800

    add commit

--- NewLibCore.Storage/SQL/EMapper/Component/Cache/RedisQueryCache.cs
+++ NewLibCore.Storage/SQL/EMapper/Component/Cache/RedisQueryCache.cs
@@ -5,14 +5,17 @@ namespace NewLibCore.Storage.SQL.Component.Cache
     {
         public RedisQueryCache()
         {
+
         }
 
         public override void Add(string key, object obj, TimeSpan? timeOut = default)
         {
+
         }
 
         public override TResult Get<TResult>(string key)
         {
+            return default;
         }
 
         public override void Remove(string key)
